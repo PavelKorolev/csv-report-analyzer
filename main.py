095d61cab48095c42c7933b2a155505168c14e2b
PavelKorolev <koroljovpi03@gmail.com>
2026-02-06 10:36:43 +0100

    Handle invalid CSV data

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import csv
 import sys
 
-with open(sys.argv[1], newline='') as csvfile:
-    reportreader = csv.reader(csvfile)
-    next(reportreader)
-    max_likes = 0
-    datum = ''
-    for row in reportreader:
-        number = int(row[1])
-        if max_likes < number:
-            max_likes = number
-            datum = row[0]
-    print(f"Maximum of likes: {max_likes}")
-    print(f"Best day: {datum}")
+if len(sys.argv) < 2:
+    print ("too few arguments")
+    exit(1)
+
+if not sys.argv[1].endswith(".csv"):
+        print("invalid format")
+        exit(1)
+
+try:
+    with open(sys.argv[1], newline='') as csvfile:
+        reportreader = csv.reader(csvfile)
+        next(reportreader)
+        max_likes = 0
+        datum = ''
+        for row in reportreader:
+            try:
+                number = int(row[1])
+                if max_likes < number:
+                    max_likes = number
+                    datum = row[0]
+            except ValueError:
+                print("Invalid data in CSV")
+                exit(1)
+        print(f"Maximum of likes: {max_likes}")
+        print(f"Best day: {datum}")
+except FileNotFoundError:
+    print("Cannot open file")
+    exit(1)
